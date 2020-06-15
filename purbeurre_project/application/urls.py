048b79dab748e4314ea24ebd4aa080f4499197b3
Gabrielle Barboteau <g.barboteau@gmail.com>
2020-06-15 17:51:55 +0200

    fixing a view bug

--- purbeurre_project/application/urls.py
+++ purbeurre_project/application/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     # path("login/", views.logout_view),
     path("logout/", views.logout_view, name="logout"),
     path("mentionslegales/", views.mentionslegales, name="mentionslegales"),
+    path('login/', auth_views.LoginView, name="login"),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='application/registration/password_reset_form.html'), name='password_reset'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='application/registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='application/registration/password_reset_done.html'), name='password_reset_done'),
