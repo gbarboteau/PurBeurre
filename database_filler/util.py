24dc24a69a37aabe7e430a1440971d511faaeb79
Gabrielle Barboteau <g.barboteau@gmail.com>
2020-06-17 17:00:53 +0200

    Reorganizing project

--- database_filler/util.py
+++
@@ -1,9 +0,0 @@
-"""Some useful functions abstract enough
-to be used into several scripts.
-"""
-def does_exist_in_dict(my_key, my_dict):
-    """Check if a key exists in a given dict."""
-    if my_key in my_dict:
-        return my_dict[my_key]
-    else:
-        return "NONE"
